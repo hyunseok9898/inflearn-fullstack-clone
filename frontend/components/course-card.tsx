5e1d65bcf578c82500f68964dc142f87d6457137
hyunseok9898 <139948998+hyunseok9898@users.noreply.github.com>
2026-06-02 21:10:10 +0900

    강의 즐겨찾기 기능 추가 (상세페이지, 강의 목록 화면)

--- frontend/components/course-card.tsx
+++ frontend/components/course-card.tsx
@@ -1,23 +1,71 @@
 "use client";
 
 import { Course } from "@/generated/openapi-client";
-import { Heart, ShoppingCart } from "lucide-react";
+import { HeartIcon, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { getLevelText } from "@/lib/level";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import * as api from "@/lib/api";
+import { cn } from "@/lib/utils";
 
 interface CourseCardProps {
   course: Course;
+  user: {
+    id: string;
+  } | null;
 }
 
-export default function CourseCard({ course }: CourseCardProps) {
+export default function CourseCard({ user, course }: CourseCardProps) {
   const router = useRouter();
+  const getMyFavoritesQuery = useQuery({
+    queryKey: ["myFavorites", user?.id],
+    queryFn: () => {
+      if (user) {
+        return api.getMyFavorites();
+      } else {
+        return null;
+      }
+    },
+  });
+
+  const isFavorite = getMyFavoritesQuery.data?.data?.find(
+    (fav) => fav.courseId === course.id,
+  );
+
   const handleFavoriteClick = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
-    alert("구현 예정");
+    if (user) {
+      if (isFavorite) {
+        removeFavoriteMutation.mutate();
+      } else {
+        addFavoriteMutation.mutate();
+      }
+    } else {
+      alert("로그인 후 이용 가능합니다.");
+    }
   };
+
+  const addFavoriteMutation = useMutation({
+    mutationFn: () => api.addFavorite(course.id),
+    onSuccess: () => {
+      getMyFavoritesQuery.refetch();
+    },
+  });
+
+  const removeFavoriteMutation = useMutation({
+    mutationFn: () => {
+      return api.removeFavorite(course.id);
+    },
+    onSuccess: () => {
+      getMyFavoritesQuery.refetch();
+    },
+  });
+
+  const isFavoriteDisabled =
+    addFavoriteMutation.isPending || removeFavoriteMutation.isPending;
 
   const handleCartClick = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -58,7 +106,13 @@ export default function CourseCard({ course }: CourseCardProps) {
             className="h-8 w-8 p-0"
             onClick={handleFavoriteClick}
           >
-            <Heart className="h-4 w-4" />
+            <HeartIcon
+              className={cn(
+                "size-4 transition-colors",
+                isFavorite ? "fill-red-500 text-red-500" : "text-gray-500",
+                isFavoriteDisabled && "cursor-not-allowed",
+              )}
+            />
           </Button>
           <Button
             size="sm"
