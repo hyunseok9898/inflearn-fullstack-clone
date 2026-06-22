87bba7ca44aaa91d74dae77fc2a43e2a1e29b356
hyunseok9898 <139948998+hyunseok9898@users.noreply.github.com>
2026-06-22 16:49:54 +0900

    [BE] NestJS Cron(Scheduler)로 일간 통계 배치 작업 구현

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b370aaf3ae164d114516a9e2b1ae01e3@o4511607603658752.ingest.us.sentry.io/4511607628562432",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
